414523d2fd6e06bd155b9fcf77ee3841ecc989c0
Michiel Gerritsen <michiel@controlaltdelete.nl>
2025-03-18 13:37:23 +0100

    Support floats #3

--- src/Data/Amount.php
+++ src/Data/Amount.php
@@ -7,7 +7,7 @@ namespace Icepay\Payment\Data;
 class Amount
 {
     public function __construct(
-        public readonly int $value,
+        public readonly float $value,
         public readonly string $currency
     ) {}
 }
